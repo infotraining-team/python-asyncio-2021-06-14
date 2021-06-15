05d808ad00865589bb096933a5c4ea2a12040af9
Leszek Tarkowski <leszek.tarkowski@gmail.com>
2021-06-15 11:46:28 +0200

    update

--- asimplefile.py
+++ asimplefile.py
@@ -10,6 +10,10 @@ async def main():
         content = await f.read()
         print(content)
 
+    async with asimplefile("hello.py") as f:
+        async for line in f:
+            print(line)
+
     # with open("hello.py") as f:
     #     content = f.read()
     #     print(content)
--- test_asimplefile.py
+++ test_asimplefile.py
@@ -1,8 +1,10 @@
 from asimplefile import asimplefile
 import pytest
 import contextlib
+import unittest.mock as mock
 
 @pytest.mark.asyncio
 async def test_getting_CM():
-    async with asimplefile("hello.py") as f:
-        assert isinstance(f, contextlib.AbstractAsyncContextManager)
+    with mock.patch('builtins.open', mock.mock_open(read_data="asdads")):
+        async with asimplefile("hello.py") as f:
+            assert isinstance(f, contextlib.AbstractAsyncContextManager)

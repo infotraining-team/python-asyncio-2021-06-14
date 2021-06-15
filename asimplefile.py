1f106c83f1e99c3ac2c3a7bff4f298540bb1eb76
Leszek Tarkowski <leszek.tarkowski@gmail.com>
2021-06-15 12:58:47 +0200

    simple file

--- asimplefile.py
+++ asimplefile.py
@@ -2,7 +2,19 @@ import asyncio
 
 # async CM
 class asimplefile:
-    pass
+    def __init__(self, filename):
+        self.filename = filename
+
+    async def __aenter__(self):
+        self.handler = await asyncio.to_thread(open, self.filename)
+        return self
+
+    async def read(self):
+        return await asyncio.to_thread(self.handler.read)
+
+    async def __aexit__(self, exc, ext, tb):
+        await asyncio.to_thread(self.handler.close)
+
 
 # target
 async def main():
@@ -10,9 +22,9 @@ async def main():
         content = await f.read()
         print(content)
 
-    async with asimplefile("hello.py") as f:
-        async for line in f:
-            print(line)
+    # async with asimplefile("hello.py") as f:
+    #     async for line in f:
+    #         print(line)
 
     # with open("hello.py") as f:
     #     content = f.read()
